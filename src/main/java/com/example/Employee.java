573b645dab7d67ac3a830d0ee491f392061d75b1
ghatikawaykole <ghatika99@gmail.com>
2021-10-14 00:58:08 +0530

    welcome the employee payroll compute

---
+++ src/main/java/com/example/Employee.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Employee {
+    int id;
+    int salary;
+    String name;
+
+    public Employee(int id, int salary, String name) {
+        this.id = id;
+        this.salary = salary;
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/example/EmployeePayrollCompute.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EmployeePayrollCompute {
+    public static void main(String args[]){
+        System.out.println("welcome the employee payroll compute");
+    }
+}
